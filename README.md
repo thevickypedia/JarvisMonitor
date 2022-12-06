0db8c2c65930f9e7e11edd9bcffa8f7aae26ab5e
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-12-06 11:23:46 -0600

    Insert meta tags Let docs be generated from a dedicated branch Send cron output to a file and add it to .gitignore Remove index.html in main branch

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 A monitor that runs in the background to report the health status of Jarvis and its processes
 
 ```bash
-* * * * * cd $HOME/JarvisMonitor && bash run.sh
+* * * * * cd $HOME/JarvisMonitor && bash run.sh >> monitor.log
 ```
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+git branch -d docs  # Delete local branch
+git push origin --delete docs  # Delete remote branch
+git checkout -b docs  # Create a new branch
 dt=$(date '+%d/%m/%Y %H:%M:%S %Z %z');
 source venv/bin/activate && python main.py
 git add "docs/index.html"
 git commit -m "Updated as of $dt"
-git push -f origin main
+git push -f origin docs
