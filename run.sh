2ea67327057103abfe674ca42e868119265de20c
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-12-06 12:53:39 -0600

    Delete local branch before push

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-# git branch -d docs  # Delete local branch
 # git push origin --delete docs  # Delete remote branch
+git branch -D docs  # Delete local branch to avoid overlap errors
 git checkout -b docs  # Create a new branch
 dt=$(date '+%d/%m/%Y %H:%M:%S %Z %z');
 source venv/bin/activate && python main.py
