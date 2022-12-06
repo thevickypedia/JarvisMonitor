7f6476e0f471a88feb4fcd01f2694e4ba912b1db
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-12-06 11:29:30 -0600

    Do not delete remote branch as it disables pages Do not delete local branch as it is not required

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-git branch -d docs  # Delete local branch
-git push origin --delete docs  # Delete remote branch
+# git branch -d docs  # Delete local branch
+# git push origin --delete docs  # Delete remote branch
 git checkout -b docs  # Create a new branch
 dt=$(date '+%d/%m/%Y %H:%M:%S %Z %z');
 source venv/bin/activate && python main.py
