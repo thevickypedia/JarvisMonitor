aef289846e5c3422eff507e5598a29a9ad18addd
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-12-31 10:24:32 -0600

    Make `Jarvis` the one source of truth for processes Remove all hard coded process names Add hyperlink to docs page in email_template.html

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import string
 import time
 from datetime import datetime
 from threading import Thread
-from typing import Dict, Union, List
+from typing import Dict, Union
 
 import jinja2
 import psutil
@@ -12,21 +12,20 @@ from gmailconnector.send_email import SendEmail
 
 from constants import FILE_PATH, NOTIFICATION, DATETIME, LOGGER, ColorCode, skip_schedule
 
+impact_lib = {}
+if os.path.isfile(os.path.join('docs', 'CNAME')):
+    with open(os.path.join('docs', 'CNAME')) as f:
+        webpage = f.read()
+else:
+    webpage = None
+
 
 def get_data() -> Union[Dict[str, int], None]:
     """Get processes mapping from Jarvis."""
-    if not os.path.isfile(FILE_PATH):
-        return
-
-    with open(FILE_PATH) as file:
-        data = yaml.load(stream=file, Loader=yaml.FullLoader) or {}
-
-    # Remove temporary processes that will stop anyway
-    if data.get('tunneling'):
-        del data['tunneling']
-    if data.get('speech_synthesizer'):
-        del data['speech_synthesizer']
-    return data
+    if os.path.isfile(FILE_PATH):
+        with open(FILE_PATH) as file:
+            data = yaml.load(stream=file, Loader=yaml.FullLoader) or {}
+        return data
 
 
 def is_running(pid) -> bool:
@@ -41,40 +40,29 @@ def publish_docs(status: dict = None):
     """Updates the docs/index.html file."""
     LOGGER.info("Updating index.html")
     t_desc, l_desc = "", ""
-    if not status:
+    if not status:  # process map is missing
         status = {"Jarvis": ColorCode.blue}
         stat_file = "maintenance.png"
         stat_text = "Process Map Unreachable"
         t_desc = "<b>Description:</b> Source feed is missing, Jarvis has been stopped for maintenance."
-    elif len(set(list(status.values()))) == 1 and set(list(status.values())) == {ColorCode.red}:
+    elif len(set(list(status.values()))) == 1 and set(list(status.values())) == {ColorCode.red}:  # all pids are red
         stat_file = "issue.png"
         stat_text = "Service disrupted by an external factor"
         t_desc = "<b>Description:</b> Source feed is present but all processes have been terminated abruptly."
-    elif status["Jarvis"] == ColorCode.red:
+    elif status["Jarvis"] == ColorCode.red:  # main process is red
         stat_file = "notice.png"
         stat_text = "Main functionality has been degraded"
         t_desc = "<b>Description:</b> Main process has degraded, making child processes rouge <i>yet active.</i>"
-    elif ColorCode.red in list(status.values()):
-        __html_list: Dict[str, List[str]] = {}
-        __keys = list(status.keys())
+    elif ColorCode.red in list(status.values()):  # one or more subprocess is red
         stat_file = "warning.png"
         stat_text = "Some components are degraded"
-        if "Automator" in __keys and status["Automator"] == ColorCode.red:
-            __html_list["automator"] = ["Cron jobs", "Home automation", "Alarms and Reminders",
-                                        "Timed sync for Meetings and Events"]
-        if "Fast Api" in __keys and status["Fast Api"] == ColorCode.red:
-            __html_list["fastapi"] = ["Offline communicator", "Robinhood report gatherer", "Jarvis UI", "Stock monitor",
-                                      "Surveillance"]
-        if "Telegram Api" in __keys and status["Telegram Api"] == ColorCode.red:
-            __html_list["telegram api"] = ["Telegram Bot"]
-        if "Wifi Connector" in __keys and status["Wifi Connector"] == ColorCode.red:
-            __html_list["wifi connector"] = ["WiFi Re-connector"]
-        for k, v in __html_list.items():
-            l_desc += f"<b>Impacted by {k}:</b><br><ul><li>{'</li><li>'.join(v)}</li></ul>"
-    else:
+        for key in status.keys():
+            if status[key] == ColorCode.red:
+                l_desc += f"<b>Impacted by {key.lower()}:</b><br><ul><li>{'</li><li>'.join(impact_lib[key])}</li></ul>"
+    else:  # all green
         stat_text = "Jarvis is up and running"
         stat_file = "ok.png"
-        if len(status) == 1:
+        if len(status) == 1:  # Limited mode
             t_desc = "<b>Description:</b> Jarvis is running in limited mode. " \
                      "All offline communicators and home automations are currently unavailable."
     with open('web_template.html') as web_temp:
@@ -89,16 +77,16 @@ def publish_docs(status: dict = None):
 def send_email(status: dict = None):
     """Sends an email notification if Jarvis is down."""
     if not status:
-        state = 'maintenance'
+        LOGGER.warning("Jarvis is in maintenance mode.")
+        return
+    if len(set(list(status.values()))) == 1 and set(list(status.values())) == {ColorCode.red}:
+        state = 'issue'
+    elif status["Jarvis"] == ColorCode.red:
+        state = 'notice'
+    elif ColorCode.red in list(status.values()):
+        state = 'warning'
     else:
-        if len(set(list(status.values()))) == 1 and set(list(status.values())) == {ColorCode.red}:
-            state = 'issue'
-        elif status["Jarvis"] == ColorCode.red:
-            state = 'notice'
-        elif ColorCode.red in list(status.values()):
-            state = 'warning'
-        else:
-            state = 'ok'
+        state = 'ok'
     if os.path.isfile(NOTIFICATION):
         with open(NOTIFICATION) as file:
             data = yaml.load(stream=file, Loader=yaml.FullLoader)
@@ -115,23 +103,21 @@ def send_email(status: dict = None):
         LOGGER.critical(auth.body)
         return
     LOGGER.info("Sending email")
-    if status:
-        with open('email_template.html') as email_temp:
-            template_data = email_temp.read()
-        template = jinja2.Template(template_data)
-        content = template.render(result=status)
-        if len(set(list(status.values()))) == 1 and set(list(status.values())) == {ColorCode.red}:
-            subject = f"Service disrupted by an external force - {DATETIME}"
-        elif status["Jarvis"] == ColorCode.red:
-            subject = f"Main functionality degraded - {DATETIME}"
-        elif ColorCode.red in list(status.values()):
-            subject = f"Some components degraded - {DATETIME}"
-        else:
-            subject = f"Jarvis is up and running - {DATETIME}"
-        response = email_obj.send_email(subject=subject, html_body=content, sender="JarvisMonitor")
+    with open('email_template.html') as email_temp:
+        template_data = email_temp.read()
+    template = jinja2.Template(template_data)
+    content = template.render(result=status, webpage=webpage)
+    if len(set(list(status.values()))) == 1 and set(list(status.values())) == {ColorCode.red}:  # all pids are red
+        subject = f"Service disrupted by an external force - {DATETIME}"
+    elif status["Jarvis"] == ColorCode.red:  # main process is red
+        subject = f"Main functionality degraded - {DATETIME}"
+    elif ColorCode.red in list(status.values()):  # one or more subprocess is red
+        subject = f"Some components degraded - {DATETIME}"
     else:
-        response = email_obj.send_email(subject=f"Process map unreachable {datetime.now().strftime('%c')}",
-                                        sender="JarvisMonitor")
+        # should never get here
+        LOGGER.critical("`notify` flag was set to True without any components being affected.")
+        return
+    response = email_obj.send_email(subject=subject, html_body=content, sender="JarvisMonitor")
     if response.ok:
         LOGGER.info("Status report has been sent.")
         with open(NOTIFICATION, 'w') as file:
@@ -153,7 +139,8 @@ def main() -> None:
         Thread(target=send_email).start()
         publish_docs()
         return
-    for func_name, pid in data.items():
+    for func_name, proc_info in data.items():
+        pid, proc_impact = proc_info
         if not isinstance(pid, int):
             LOGGER.warning(f"{func_name} [{pid}] is invalid.")
             continue
@@ -166,6 +153,7 @@ def main() -> None:
             func_name = string.capwords(func_name.replace('_', ' ')).replace('api', 'API')
             status[func_name] = ColorCode.red
             notify = True
+        impact_lib[func_name] = proc_impact
     if notify:
         Thread(target=send_email, kwargs={"status": status}).start()
     elif os.path.isfile(NOTIFICATION):
